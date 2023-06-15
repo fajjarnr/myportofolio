cab585ec129a5bbfd3e68a6b44b3ea74b1698fcc
fajjarnr <zfajart@gmail.com>
2023-06-15 17:18:17 +0700

    images

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -231,7 +231,7 @@ export default function Home({ articles }) {
   return (
     <>
       <Head>
-        <title>Fajar Nur Rohman - Developer, DevOps & Cloud Enthusiast.</title>
+        <title>Fajar Nur Rohman - DevOps & Cloud Enthusiast.</title>
         <meta
           name="description"
           content="I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
@@ -240,7 +240,7 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Developer, DevOps & Cloud Enthusiast.
+            DevOps & Cloud Enthusiast.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Fajar, a developer based in Pemalang City. I have experience as
